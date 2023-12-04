0c6301bb592bf722ab88dcd62f9ce8729d550066
catherine-bao <142851995+catherine-bao@users.noreply.github.com>
2023-12-03 18:18:35 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const imgContainer = document.querySelector('.image_1');
 console.log(imgContainer);
 
-// if(imgContainer){
-//     console.log("inside")
-
-//     $(imgContainer).slick({
-//         infinite: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 3
-//         // variablewidth:true
-//         // centerMode:true
-//         // prevArrow: $(backbutton)
-//         // nextArrow: $(forwardbutton)
-//     });
-
-// }
 
 
 
+if(imgContainer){
+    console.log("inside")
+
+    $(imgContainer).slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+              breakpoint: 431,
+              settings:"unslick"
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        
+    });
+    
+}
